780defc9e21501385493240f4bdba46f1cbf3b6d
hatsune-miku <l2a1knla@gmail.com>
2022-07-17 15:05:10 -0230

    新 建 文 件 夹

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+import 'element-react'
+import 'element-theme-default'
+import { Button } from 'element-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type='primary'>Learn React</Button>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App
